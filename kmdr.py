b91e8dd54ef6f326fc8c8e38879a6ee5cd15f192
chrisis <chrisis58@outlook.com>
2025-06-29 19:27:38 +0800

    feat: 实现框架内的下载指令

---
+++ kmdr.py
@@ -0,0 +1,30 @@
+from core import *
+from module import *
+
+def main():
+    parser = argument_parser()
+    args = parser.parse_args()
+
+    if args.command == 'login':
+        ...
+
+    elif args.command == 'status':
+        ...
+
+    elif args.command == 'download':    
+
+        if not AUTHENTICATOR.get(args).authenticate():
+            print("Authentication failed. Please check your credentials.")
+            return
+
+        book, volumes = LISTERS.get(args).list()
+
+        volumes = PICKERS.get(args).pick(volumes)
+
+        DOWNLOADER.get(args).download(book, volumes)
+
+    else:
+        parser.print_help()
+
+if __name__ == '__main__':
+    main()
