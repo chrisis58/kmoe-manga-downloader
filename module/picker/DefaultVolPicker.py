28b59f7ea2de6f2442f5dd8c7ab1ccdc98cf6434
chrisis <chrisis58@foxmail.com>
2025-06-30 21:45:32 +0800

    chore: 优化命令行中选项的展示

--- module/picker/DefaultVolPicker.py
+++ module/picker/DefaultVolPicker.py
@@ -7,9 +7,9 @@ class DefaultVolPicker(Picker):
         super().__init__(*args, **kwargs)
 
     def pick(self, volumes: list[VolInfo]) -> list[VolInfo]:
-        print("\t卷类型\t卷名\t页数\t大小(MB)")
+        print("\t卷类型\t页数\t大小(MB)\t卷名")
         for v in range(len(volumes)):
-            print(f"[{v}]\t{volumes[v].vol_type.value}\t{volumes[v].name}\t{volumes[v].pages}\t{volumes[v].size:.2f}")
+            print(f"[{v}]\t{volumes[v].vol_type.value}\t{volumes[v].pages}\t{volumes[v].size:.2f}\t{volumes[v].name}")
 
         choosed = input("choose a volume to download (use comma to select multiple volumes or just 'all'):\n")
 
