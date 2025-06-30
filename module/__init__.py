76ed68a72e0adb30b38d6d05b6444bf237a64e0a
chrisis <chrisis58@foxmail.com>
2025-06-30 13:06:08 +0800

    feat: 添加从用户关注获取书籍信息的 lister

--- module/__init__.py
+++ module/__init__.py
@@ -1,4 +1,4 @@
 from .authenticator import CookieAuthenticator, LoginAuthenticator
-from .lister import BookUrlLister
+from .lister import BookUrlLister, FollowedBookLister
 from .picker import ArgsFilterPicker
 from .downloader import DirectDownloader, ReferViaDownloader
