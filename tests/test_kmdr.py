e3a96b02555e27a0a331ff534afaed6328c3d9dc
chrisis <chrisis58@foxmail.com>
2025-07-02 15:34:37 +0800

    feat: 为单元测试添加断言

--- tests/test_kmdr.py
+++ tests/test_kmdr.py
@@ -1,4 +1,4 @@
-from os import environ
+import os
 
 import unittest
 from argparse import Namespace
@@ -8,28 +8,28 @@ from module import *
 
 from kmdr import main as kmdr_main
 
-BASE_DIR = environ.get('KMDR_TEST_DIR', './tests')
+BASE_DIR = os.environ.get('KMDR_TEST_DIR', './tests')
 
 class TestKmdr(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
         from shutil import rmtree
-        from os import path
-
         test_methods = [method for method in dir(cls) if method.startswith('test_')]
 
         for method in test_methods:
             dir_path = f"{BASE_DIR}/{method}"
             
-            if path.exists(dir_path):
+            if os.path.exists(dir_path):
                 rmtree(dir_path)
 
     def test_download_multiple_volumes(self):
+        dest = f'{BASE_DIR}/{self.test_download_multiple_volumes.__name__}'
+
         kmdr_main(
             Namespace(
                 command='download',
-                dest=f'{BASE_DIR}/{self.test_download_multiple_volumes.__name__}',
+                dest=dest,
                 book_url='https://kox.moe/c/51043.htm',
                 book_name=None,
                 volume='all',
@@ -39,17 +39,37 @@ class TestKmdr(unittest.TestCase):
             )
         )
 
+        assert len(sub_dir := os.listdir(dest)) == 1, "Expected one subdirectory in the destination"
+        assert os.path.isdir(os.path.join(dest, book_dir := sub_dir[0])), "Expected the subdirectory to be a directory"
+        assert len(os.listdir(os.path.join(dest, book_dir))) == 3, "Expected 3 volumes to be downloaded"
+
+        total_size = sum(
+            os.path.getsize(os.path.join(dest, book_dir, f)) for f in os.listdir(os.path.join(dest, book_dir)) if os.path.isfile(os.path.join(dest, book_dir, f))
+        )
+        assert total_size < 3 * 0.5 * 1024 * 1024, "Total size of downloaded files exceeds 0.5 MB"
+
     def test_download_volume_with_callback(self):
+        dest = f'{BASE_DIR}/{self.test_download_volume_with_callback.__name__}'
+
         kmdr_main(
             Namespace(
                 command='download',
-                dest=f'{BASE_DIR}/{self.test_download_volume_with_callback.__name__}',
+                dest=dest,
                 book_url='https://kox.moe/c/51043.htm',
                 book_name=None,
                 volume='all',
                 max_size=0.4,
                 limit=1,
                 retry=3,
-                callback="echo 'CALLBACK: {b.name} {v.name} has been downloaded!'"
+                callback="echo 'CALLBACK: {b.name} {v.name} has been downloaded!'" + f" > {dest}/callback.log"
             )
         )
+
+        assert len(files := os.listdir(dest)) == 2, "Expected one subdirectory and one callback log file in the destination"
+        assert 'callback.log' in files, "Expected callback log file to be present"
+        with open(os.path.join(dest, 'callback.log'), 'r') as f:
+            log_content = f.read()
+            assert "CALLBACK:" in log_content, "Expected callback log to contain the correct message"
+        files.remove('callback.log')
+        assert os.path.isdir(os.path.join(dest, book_dir := files[0])), "Expected the subdirectory to be a directory"
+        assert len(os.listdir(os.path.join(dest, book_dir))) == 1, "Expected 1 volume to be downloaded"
