c3fe504910e8663f1af280c74cad3e613f5db5ef
chrisis <chrisis58@foxmail.com>
2025-07-02 20:35:52 +0800

    feat: 添加 cloudscraper 的支持

--- module/downloader/ReferViaDownloader.py
+++ module/downloader/ReferViaDownloader.py
@@ -2,17 +2,27 @@ from core import Downloader, VolInfo, DOWNLOADER, BookInfo
 
 from .utils import download_file, safe_filename
 
+try:
+    import cloudscraper
+except ImportError:
+    cloudscraper = None
+
 @DOWNLOADER.register(order=10)
 class ReferViaDownloader(Downloader):
     def __init__(self, dest, callback=None, retry=3, num_workers=1, *args, **kwargs):
         super().__init__(dest, callback, retry, num_workers, *args, **kwargs)
+
+        if cloudscraper:
+            self._scraper = cloudscraper.create_scraper()
+        else:
+            self._scraper = None
 
     def _download(self, book: BookInfo, volume: VolInfo, retry: int):
         sub_dir = safe_filename(book.name)
         download_path = f'{self._dest}/{sub_dir}'
 
         download_file(
-            self._session,
+            self._session if not self._scraper else self._scraper,
             self.fetch_download_url(book, volume),
             download_path,
             f'[Kmoe][{book.name}][{volume.name}].epub',
