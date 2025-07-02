aa36eff1c6f375166a82016c9ba1be703dd132ca
chrisis <chrisis58@foxmail.com>
2025-07-02 19:52:05 +0800

    fix: 删除错误引入

--- core/__init__.py
+++ core/__init__.py
@@ -1,6 +1,5 @@
 from .bases import Authenticator, Lister, Picker, Downloader
 from .structure import VolInfo, BookInfo, VolumeType
-from .utils import download_file
 from .bases import AUTHENTICATOR, LISTERS, PICKERS, DOWNLOADER
 
 from .defaults import argument_parser
