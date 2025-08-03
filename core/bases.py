40829d0e15839335782e72d28a80b05ce13855fb
chrisis <chrisis58@foxmail.com>
2025-08-03 11:59:24 +0800

    fix: 保留认证时的代理设置

--- core/bases.py
+++ core/bases.py
@@ -3,8 +3,8 @@ import os
 from typing import Callable, Optional
 
 from .registry import Registry
-from .structure import VolInfo, BookInfo, Config
-from .utils import get_singleton_session, construct_callback, no_proxy
+from .structure import VolInfo, BookInfo
+from .utils import get_singleton_session, construct_callback
 from .defaults import Configurer as InnerConfigurer
 
 class SessionContext:
@@ -37,7 +37,10 @@ class Authenticator(SessionContext, ConfigContext):
                 'http': proxy,
             })
 
-    @no_proxy
+    # 在使用代理登录时，可能会出现问题，但是现在还不清楚是不是代理的问题。
+    # 主站正常情况下不使用代理也能登录成功。但是不排除特殊的网络环境下需要代理。
+    # 所以暂时保留代理登录的功能，如果后续确认是代理的问题，可以考虑启用 @no_proxy 装饰器。
+    # @no_proxy
     def authenticate(self) -> bool:
         return self._authenticate()
 
