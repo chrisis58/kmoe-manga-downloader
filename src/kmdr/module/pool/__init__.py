cd30398b3d6e8b09a54099fc8eaea0aba25998c6
chrisis <chrisis58@outlook.com>
2026-01-15 17:03:35 +0800

    fix: 添加 __all__ 以优化依赖导入

--- src/kmdr/module/pool/__init__.py
+++ src/kmdr/module/pool/__init__.py
@@ -3,3 +3,11 @@ from .PoolLister import PoolLister
 from .PoolCredRemover import PoolCredRemover
 from .PoolCredSwitcher import PoolCredSwitcher
 from .PoolCredUpdator import PoolCredUpdator
+
+__all__ = [
+    "PoolInsertionHandler",
+    "PoolLister",
+    "PoolCredRemover",
+    "PoolCredSwitcher",
+    "PoolCredUpdator",
+]
